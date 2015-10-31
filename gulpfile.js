075426e18787cfde12c8d437a5e84288e5066341
Renato França <renatofrancarpmf@gmail.com>
2015-10-31 20:42:34 -0300

    Revert "Added watch files for gulp"
    This reverts commit 3328f625dc756480985f477bdb4975d38e3b787a.
    
    	modified:   gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,18 @@ var concatCss   = require('gulp-concat-css');
 var compressor  = require('gulp-compressor');
 var sass        = require('gulp-sass');
 
-var paths = {
-    sass:       './plugins/MaterialDesignLite/webroot/sass/*.scss',
-    css:        './plugins/MaterialDesignLite/webroot/css/output/*.css',
-    scripts:    './bower_components/material-design-lite/material.min.js',
-    fonts:      './bower_components/octicons/octicons/*.{ttf,woff,eof,svg}',
-};
-
 /** Sass generate */
 gulp.task('sass', function () {
-    gulp.src(paths.sass)
+    gulp.src([
+            './plugins/MaterialDesignLite/webroot/sass/*.scss'
+        ])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./plugins/MaterialDesignLite/webroot/css/output/'));
 });
 
 /** JavaScript generate */
 gulp.task('js', function() {
-    gulp.src(paths.scripts)
+    gulp.src('./bower_components/material-design-lite/material.min.js')
         .pipe(concat('all.js'))
         .pipe(compressor())
         .pipe(gulp.dest('./plugins/MaterialDesignLite/webroot/js/'));
@@ -29,7 +24,7 @@ gulp.task('js', function() {
 
 /** CSS generate */
 gulp.task('css', function () {
-    gulp.src(paths.css)
+    gulp.src('./plugins/MaterialDesignLite/webroot/css/output/*.css')
         .pipe(concatCss('all.min.css'))
         .pipe(compressor({
             'executeOption': {
@@ -41,19 +36,14 @@ gulp.task('css', function () {
 
 /** Octicons generate **/
 gulp.task('copyfonts', function() {
-    gulp.src(paths.fonts)
+    gulp.src('./bower_components/octicons/octicons/*.{ttf,woff,eof,svg}')
     .pipe(gulp.dest('./plugins/MaterialDesignLite/webroot/css/separate/octicons/'));
 });
 
 gulp.task('octicons', ['copyfonts'], function() {
-    gulp.src(['./bower_components/octicons/octicons/octicons.scss'])
+    gulp.src([
+            './bower_components/octicons/octicons/octicons.scss'
+        ])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./plugins/MaterialDesignLite/webroot/css/separate/octicons/'));
-});
-
-/** Watch files **/
-gulp.task('watch', function() {
-    gulp.watch(paths.sass, ['sass']);
-    gulp.watch(paths.scripts, ['js']);
-    gulp.watch(paths.css, ['css']);
 });
