03eb02eed5f54d5a5d8bd1a82fd646a16d6adc52
Renato França <renato.franca@frannca.com.br>
2018-02-08 19:41:26 -0300

    Created layout and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+
+    ngOnInit() {
+        console.log('ngAfterContentInit');
+        // fix menu when passed
+        jQuery('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function() {
+                    console.log('onBottomPassed');
+                    jQuery('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function() {
+                    jQuery('.fixed.menu').transition('fade out');
+                }
+            })
+        ;
+
+        // create sidebar and attach to menu open
+        // jQuery('.ui.sidebar')
+        //     .sidebar('attach events', '.toc.item')
+        // ;
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
