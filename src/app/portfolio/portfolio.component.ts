bf13aefebd595f584a2dccb45a449665eb6e5463
Renato França <renato.franca@frannca.com.br>
2018-02-12 14:09:55 -0300

    Added Diocesano project in Portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+declare var jQuery: any;
 
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+    selector: 'app-portfolio',
+    templateUrl: './portfolio.component.html',
+    styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent implements AfterContentInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngAfterContentInit() {
+        setTimeout(() => {
+            const header = jQuery('#portfolio').position();
+            console.log(header.top);
+            jQuery('body').animate({ scrollTop: header.top + 10 }, 1000);
+        }, 500);
+    }
 
 }
